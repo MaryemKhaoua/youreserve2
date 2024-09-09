156bc3e640106e07ac5d385e40bdffb43e24db95
Maryem khaoua <maryem.khaoua@gmail.com>
2024-09-09 15:48:15 +0100

    make connection and create architetcture for the project

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        System.out.println("Hello world!");
+        Connection connection = configConx.getInstance().getConnection();
+
+    }
+}
---
+++ src/java/connection/configConx.java
@@ -0,0 +1,54 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class configConx {
+    private static configConx instance;
+    private Connection connection;
+
+    // Constructeur privé pour Singleton
+    private configConx() throws SQLException {
+        String DATABASE_URL = System.getenv("DATABASE_URL");
+        String USER = System.getenv("USER");
+        String PASSWORD = System.getenv("PASSWORD");
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            this.connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
+            System.out.println("Connexion établie avec succès !");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver PostgreSQL JDBC non trouvé : " + e.getMessage());
+            throw new SQLException(e);
+        } catch (SQLException e) {
+            System.out.println("Erreur de connexion à la base de données : " + e.getMessage());
+            throw e;
+        }
+    }
+
+    // Méthode pour obtenir l'instance Singleton
+    public static configConx getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new configConx();
+        } else if (instance.getConnection().isClosed()) {
+            instance = new configConx();
+        }
+        return instance;
+    }
+
+    // Méthode pour obtenir la connexion
+    public Connection getConnection() {
+        return connection;
+    }
+
+    // Méthode pour fermer la connexion
+    public static void closeConnection() {
+        if (instance != null && instance.getConnection() != null) {
+            try {
+                instance.getConnection().close();
+                System.out.println("Connexion fermée.");
+            } catch (SQLException e) {
+                System.out.println("Erreur lors de la fermeture de la connexion : " + e.getMessage());
+            }
+        }
+    }
+}
